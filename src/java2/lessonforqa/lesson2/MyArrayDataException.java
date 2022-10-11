61653c50eb3a8fd847a91ce55e2a5a4863a75371
Nastasia_Shvetsova <NastasiaShvetsova@yandex.ru>
2022-10-11 16:40:53 +0300

    removed

--- src/java2/lessonforqa/lesson2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package java2.lessonforqa.lesson2;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
-
--- src/java2/lessonforqa/lesson2/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package java2.lessonforqa.lesson2;
-
-class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
